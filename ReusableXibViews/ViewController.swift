354647362e065e22c018e78a2dbb6a9c0814b2e7
Matthias Lamoureux <matthias@extrabat.com>
2020-01-13 14:40:35 +0100

    Initial Commit

---
+++ ReusableXibViews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReusableXibViews
+//
+//  Created by Matthias Lamoureux on 13/01/2020.
+//  Copyright © 2020 QSC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
